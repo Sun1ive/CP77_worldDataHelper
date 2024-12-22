f7fd5a6c8e4eccb7bac84905db5420764a13ca6c
Sunlive <sunliveua@gmail.com>
2024-12-22 23:31:41 +0200

    chore: cleanup

--- init.lua
+++ init.lua
@@ -1,20 +1,7 @@
 -- require("modules/GameSession")
--- mod info
 mod = {
     ready = false
 }
-
--- print on load
-print('My Mod is loaded!')
-
--- onInit event
-registerForEvent('onInit', function()
-    -- set as ready
-    mod.ready = true
-
-    -- print on initialize
-    print('My Mod is initialized!')
-end)
 
 registerForEvent('onDraw', function()
     local player = Game.GetPlayer()
