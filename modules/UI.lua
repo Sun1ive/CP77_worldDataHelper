28646b993fa426eb0d2fc0799f3b8c4b6528766a
Sunlive <sunliveua@gmail.com>
2024-12-24 13:16:30 +0200

    feat: added basic recorder

--- modules/UI.lua
+++ modules/UI.lua
@@ -2,16 +2,27 @@
 ---@field from Vector4?
 ---@field to Vector4?
 ---@field result Vector4?
----
 ---@class UI
 ---@field render function
 ---@field calculator Calculator
+---@field player PlayerPuppet?
+---@field viewSize number
 UI = {
+    version = "0.0.8",
     Utils = require('modules/Utils'),
+    Cron = require('modules/Cron'),
+
+    startedRecording = false,
 
     tppToggle = false,
     radioChannel = 0,
     formatter = "%.9f",
+
+    player = nil,
+    viewSize = 0,
+
+    ---@type Vector4[] # points is an array of Vector4
+    points = {},
 
     calculator = {
         from = Vector4.new(0, 0, 0, 1),
@@ -26,11 +37,123 @@ function UI:drawField(name, prop)
     self.Utils:tooltip(name)
 end
 
+function UI:renderRecorder()
+    if ImGui.CollapsingHeader("Recorder") then
+        ImGui.PushItemWidth(300 * self.viewSize)
+        if not self.startedRecording then
+            if ImGui.Button("Start record") then
+                self.startedRecording = true
+            end
+        else
+            if ImGui.Button("Add point") then
+                local pos = self.player:GetWorldPosition()
+                table.insert(self.points, pos)
+                print(string.format("Recorded Position: x=%.2f, y=%.2f, z=%.2f, w=%.2f", pos.x, pos.y, pos.z, pos.w))
+            end
+            ImGui.SameLine()
+            if ImGui.Button("Stop record") then
+                self.startedRecording = false
+                for index, pos in ipairs(self.points) do
+                    print(string.format("Recorded Position: x=%.2f, y=%.2f, z=%.2f, w=%.2f", pos.x, pos.y, pos.z, pos.w))
+                end
+            end
+
+        end
+
+        ImGui.PopItemWidth()
+    end
+end
+
+function UI:renderOffsetsTab()
+    if ImGui.CollapsingHeader("offsets") then
+        ImGui.PushItemWidth(300 * self.viewSize)
+        ImGui.Text("From X")
+        ImGui.SameLine()
+        ImGui.Text("From Y")
+        ImGui.SameLine()
+        ImGui.Text("From Z")
+        ImGui.PopItemWidth()
+
+        ImGui.PushItemWidth(100 * self.viewSize)
+        self.calculator.from.x = Utils.handleVector4Input("From", "x", self.calculator.from)
+        ImGui.SameLine()
+        self.calculator.from.y = Utils.handleVector4Input("From", "y", self.calculator.from)
+        ImGui.SameLine()
+        self.calculator.from.z = Utils.handleVector4Input("From", "z", self.calculator.from)
+        ImGui.PopItemWidth()
+
+        ImGui.PushItemWidth(300 * self.viewSize)
+        ImGui.Text("To X")
+        ImGui.SameLine()
+        ImGui.Text("To Y")
+        ImGui.SameLine()
+        ImGui.Text("To Z")
+        ImGui.PopItemWidth()
+
+        ImGui.PushItemWidth(100 * self.viewSize)
+        self.calculator.to.x = Utils.handleVector4Input("To", "x", self.calculator.to)
+        ImGui.SameLine()
+        self.calculator.to.y = Utils.handleVector4Input("To", "y", self.calculator.to)
+        ImGui.SameLine()
+        self.calculator.to.z = Utils.handleVector4Input("To", "z", self.calculator.to)
+        ImGui.PopItemWidth()
+
+        ImGui.PushItemWidth(300 * self.viewSize)
+        ImGui.Text("Result X")
+        ImGui.SameLine()
+        ImGui.Text("Result Y")
+        ImGui.SameLine()
+        ImGui.Text("Result Z")
+        ImGui.PopItemWidth()
+
+        ImGui.PushItemWidth(100 * self.viewSize)
+        local result = Utils.calculateVectorDifference(self.calculator.from, self.calculator.to)
+        self:drawField("ResultX", result.x)
+        ImGui.SameLine()
+        self:drawField("ResultY", result.y)
+        ImGui.SameLine()
+        self:drawField("ResultZ", result.z)
+        ImGui.PopItemWidth()
+    end
+    -- MOVE TO Offsets tab
+    -- ImGui.SameLine()
+    -- if ImGui.Button("Save Position") then
+    --     if player then
+    --         self.savedPosition = player.GetWorldPosition(player)
+    --         print(self.savedPosition.x .. "\n" .. self.savedPosition.y .. "\n" .. self.savedPosition.z)
+    --     end
+    -- end
+    -- ImGui.SameLine()
+    -- if self.savedPosition then
+    --     if ImGui.Button("Calc offset") then
+    --         self.positionOffset = self.Utils:calculateVectorDifference(position, self.savedPosition)
+    --     end
+    -- end
+    -- ImGui.PopItemWidth()
+
+    -- ImGui.Separator()
+
+    -- if self.positionOffset then
+    --     ImGui.PushItemWidth(100 * viewSize)
+    --     self:drawField("X-offset", self.positionOffset.x)
+    --     ImGui.SameLine()
+    --     self:drawField("Y-offset", self.positionOffset.y)
+    --     ImGui.SameLine()
+    --     self:drawField("Z-offset", self.positionOffset.z)
+    --     ImGui.PopItemWidth()
+    -- end
+end
+
 function UI:render()
-    local player = Game.GetPlayer()
-    local position = player:GetWorldPosition()
-    local orient = player:GetWorldOrientation()
-    local viewSize = self.Utils:getViewSize()
+    if not self.player then
+        self.player = Game.GetPlayer()
+    end
+    if self.viewSize == 0 then
+        self.viewSize = self.Utils:getViewSize()
+    end
+
+    local position = self.player:GetWorldPosition()
+    local orient = self.player:GetWorldOrientation()
 
     ImGui.SetNextWindowPos(100, 100, ImGuiCond.FirstUseEver) -- set window position x, y
     ImGui.SetNextWindowSize(100, 100, ImGuiCond.FirstUseEver) -- set window size w, h
@@ -38,7 +161,7 @@ function UI:render()
     if ImGui.Begin("World Position Helper") then
         ImGui.PushStyleColor(ImGuiCol.Text, 0xFFA5A19B)
 
-        ImGui.PushItemWidth(100 * viewSize)
+        ImGui.PushItemWidth(100 * self.viewSize)
         self.radioChannel = ImGui.RadioButton("%.9f", self.radioChannel, 0)
         ImGui.SameLine()
         self.radioChannel = ImGui.RadioButton("%.2f", self.radioChannel, 1)
@@ -52,7 +175,7 @@ function UI:render()
 
         ImGui.Separator()
 
-        ImGui.PushItemWidth(100 * viewSize)
+        ImGui.PushItemWidth(100 * self.viewSize)
         self:drawField("X", position.x)
         ImGui.SameLine()
         self:drawField("Y", position.y)
@@ -60,7 +183,7 @@ function UI:render()
         self:drawField("Z", position.z)
         ImGui.PopItemWidth()
 
-        ImGui.PushItemWidth(80 * viewSize)
+        ImGui.PushItemWidth(80 * self.viewSize)
         self:drawField("I", orient.i)
         ImGui.SameLine()
         self:drawField("J", orient.j)
@@ -72,7 +195,7 @@ function UI:render()
 
         ImGui.Separator()
 
-        ImGui.PushItemWidth(80 * viewSize)
+        ImGui.PushItemWidth(80 * self.viewSize)
         if ImGui.Button("Toggle TPP") then
             self.tppToggle = not self.tppToggle
             if self.tppToggle then
@@ -83,85 +206,13 @@ function UI:render()
                 Game.GetPlayer():GetFPPCameraComponent():SetLocalOrientation(Quaternion.new(0.0, 0.0, 0.0, 1.0))
             end
         end
-        ImGui.SameLine()
-        if ImGui.Button("Save Position") then
-            if player then
-                self.savedPosition = player.GetWorldPosition(player)
-                print(self.savedPosition.x .. "\n" .. self.savedPosition.y .. "\n" .. self.savedPosition.z)
-            end
-        end
-        ImGui.SameLine()
-        if self.savedPosition then
-            if ImGui.Button("Calc offset") then
-                self.positionOffset = self.Utils:calculateVectorDifference(position, self.savedPosition)
-            end
-        end
-        ImGui.PopItemWidth()
 
         ImGui.Separator()
 
-        if self.positionOffset then
-            ImGui.PushItemWidth(100 * viewSize)
-            self:drawField("X-offset", self.positionOffset.x)
-            ImGui.SameLine()
-            self:drawField("Y-offset", self.positionOffset.y)
-            ImGui.SameLine()
-            self:drawField("Z-offset", self.positionOffset.z)
-            ImGui.PopItemWidth()
-        end
+        self:renderOffsetsTab()
 
-        ImGui.Separator()
+        self:renderRecorder()
 
-        if ImGui.CollapsingHeader("offsets") then
-            ImGui.PushItemWidth(300 * viewSize)
-            ImGui.Text("From X")
-            ImGui.SameLine()
-            ImGui.Text("From Y")
-            ImGui.SameLine()
-            ImGui.Text("From Z")
-            ImGui.PopItemWidth()
-
-            ImGui.PushItemWidth(100 * viewSize)
-            self.calculator.from.x = Utils:handleVector4Input("From", "x", self.calculator.from)
-            ImGui.SameLine()
-            self.calculator.from.y = Utils:handleVector4Input("From", "y", self.calculator.from)
-            ImGui.SameLine()
-            self.calculator.from.z = Utils:handleVector4Input("From", "z", self.calculator.from)
-            ImGui.PopItemWidth()
-
-            ImGui.PushItemWidth(300 * viewSize)
-            ImGui.Text("To X")
-            ImGui.SameLine()
-            ImGui.Text("To Y")
-            ImGui.SameLine()
-            ImGui.Text("To Z")
-            ImGui.PopItemWidth()
-
-            ImGui.PushItemWidth(100 * viewSize)
-            self.calculator.to.x = Utils:handleVector4Input("To", "x", self.calculator.to)
-            ImGui.SameLine()
-            self.calculator.to.y = Utils:handleVector4Input("To", "y", self.calculator.to)
-            ImGui.SameLine()
-            self.calculator.to.z = Utils:handleVector4Input("To", "z", self.calculator.to)
-            ImGui.PopItemWidth()
-
-            ImGui.PushItemWidth(300 * viewSize)
-            ImGui.Text("Result X")
-            ImGui.SameLine()
-            ImGui.Text("Result Y")
-            ImGui.SameLine()
-            ImGui.Text("Result Z")
-            ImGui.PopItemWidth()
-
-            ImGui.PushItemWidth(100 * viewSize)
-            local result = Utils:calculateVectorDifference(self.calculator.from, self.calculator.to)
-            self:drawField("ResultX", result.x)
-            ImGui.SameLine()
-            self:drawField("ResultY", result.y)
-            ImGui.SameLine()
-            self:drawField("ResultZ", result.z)
-            ImGui.PopItemWidth()
-        end
     end
     ImGui.End()
 
--- modules/Utils.lua
+++ modules/Utils.lua
@@ -8,7 +8,7 @@ Utils = {}
 ---@param v1 Vector4 The first vector
 ---@param v2 Vector4 The second vector
 ---@return Vector4 A new Vector4 representing the difference between v1 and v2
-function Utils:calculateVectorDifference(v1, v2)
+function Utils.calculateVectorDifference(v1, v2)
     -- Ensure both vectors are provided
     if not v1 or not v2 then
         error("Both vectors must be provided.")
@@ -26,22 +26,22 @@ function Utils:calculateVectorDifference(v1, v2)
 end
 
 ---@return number
-function Utils:getViewSize()
+function Utils.getViewSize()
     return ImGui.GetFontSize() / 15
 end
 
 ---@param x any
 ---@param y any
-function Utils:setCursorRelative(x, y)
+function Utils.setCursorRelative(x, y)
     local xC, yC = ImGui.GetMousePos()
-    local viewSize = Utils:getViewSize()
+    local viewSize = Utils.getViewSize()
     ImGui.SetNextWindowPos(xC + x * viewSize, yC + y * viewSize, ImGuiCond.Always)
 end
 
 ---@param text any
-function Utils:tooltip(text)
+function Utils.tooltip(text)
     if ImGui.IsItemHovered() then
-        Utils:setCursorRelative(8, 8)
+        Utils.setCursorRelative(8, 8)
 
         ImGui.SetTooltip(text)
     end
@@ -52,7 +52,7 @@ end
 ---@param prop string The property of the Vector4 to edit
 ---@param vector Vector4 The Vector4 object being edited
 ---@return float
-function Utils:handleVector4Input(name, prop, vector)
+function Utils.handleVector4Input(name, prop, vector)
     -- Create a UI input box
     local input, updated = ImGui.InputTextWithHint(
         "##" .. name .. prop,
